a79852995c8acd253ec90aa5254e430ab531bef5
Sarthak Walake <sarthakwalake7424@gmail.com>
2026-02-26 16:13:13 +0530

    Stored Procedure

--- Assessments/Stored_Procedure.sql
+++ Assessments/Stored_Procedure.sql
@@ -50,5 +50,58 @@ EXECUTE sp_MathTutor 5 , 6 , @answer OUTPUT
 SELECT CONCAT('THE RESULT IS  ',@answer)
 
 
+USE Northwind
+
+SELECT * FROM Employees
+
+-- CREATING NEW PROCEDURE
+CREATE PROCEDURE sp_NewEmployee
+@LastName NVARCHAR(20),
+@FirstName NVARCHAR(20) 
+AS
+ BEGIN 
+   INSERT INTO Employees(LastName , FirstName)
+   VALUES (@LastName , @FirstName)
+   RETURN SCOPE_IDENTITY()
+   END
+
+   -- CREATING ANOTHER NEW PROCEDURE THAT CALL PREVIOUS STORED PROCEDURE
+ CREATE PROCEDURE sp_CallNewEmployee @lastname VARCHAR(10) , @firstname VARCHAR(10)
+  AS
+   BEGIN
+    DECLARE @NewEmployeeId INT 
+    EXECUTE @NewEmployeeId = sp_NewEmployee
+    @LastName = @lastname , @FirstName = @firstname
+    SELECT e.EmployeeID , e.LastName , e.FirstName
+    FROM Employees e
+    WHERE EmployeeID = @NewEmployeeId
+   END
+
+
+-- CALLING PROCEDURE
+ EXECUTE sp_CallNewEmployee 'Vinu' , 'naik'
+
+
+ -- CREATING STORED PROCEDURE FOR UPDATE
+CREATE PROCEDURE sp_UpdateEmployee
+ @employeeId INT,
+ @address VARCHAR(20)
+ AS
+   BEGIN
+      UPDATE Employees SET Address = @address
+      WHERE EmployeeID = @employeeId;
+
+      SELECT * FROM Employees
+  END
+
+
+-- Calling procedure    
+EXECUTE sp_UpdateEmployee 10,'pune'
+
+
+-- CREATING STORED PROCEDURE FOR DELETE 
+CREATE PROCEDURE
+      
+
 
 
